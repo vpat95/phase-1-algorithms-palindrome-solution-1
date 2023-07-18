37958159a49c8beb604d00c065e1ed3fcddb522c
Vivek Patel <vpatel360@gmail.com>
2023-07-18 09:15:36 -0400

    commit tree fd0afd154cc9918c30e6fcc9731008ee62030d48

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function reverseWord(word){
+  return word.split('').reverse().join('')
+}
+
+function isPalindrome(word){
+  const reversedWord = reverseWord(word)
+  return word === reversedWord
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  
+  - reverse string input
+
+  - if reverse string input ==== initial string 
+      return true 
+    else 
+      false
 */
 
 /*
@@ -15,6 +28,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
